b4b601c0dfed0329c27205b22f7bb098a6497a2a
gevorggasparyan <gevorg.gasparyan@esterox.am>
2024-06-10 11:52:59 +0400

    fix: date time format

--- src/utils/client/date.ts
+++ src/utils/client/date.ts
@@ -1,6 +1,13 @@
 export const formatFullDate = (date: Date) => {
-  return new Date(date.getTime() - date.getTimezoneOffset() * 60000)
-    .toISOString()
-    .split(".")[0]
-    .replace(/[T:]/g, ":");
+  const addLeadingZero = (num) => (num < 10 ? `0${num}` : num);
+
+  const day = addLeadingZero(date.getDate());
+  const month = addLeadingZero(date.getMonth() + 1); // Months are zero-based
+  const year = date.getFullYear();
+
+  const hours = addLeadingZero(date.getHours());
+  const minutes = addLeadingZero(date.getMinutes());
+  const seconds = addLeadingZero(date.getSeconds());
+
+  return `${day}-${month}-${year} : ${hours}:${minutes}:${seconds}`;
 };
